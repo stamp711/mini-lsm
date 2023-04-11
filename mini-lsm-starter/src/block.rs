8ec93f41bbc9336298e3779def7358803cf58ac5
stamp711 <stamp1024@gmail.com>
2023-04-11 22:42:59 +0800

    Block Builder

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,12 +1,11 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 pub use iterator::BlockIterator;
+
+const SIZEOF_U16: usize = std::mem::size_of::<u16>();
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted
 /// key-value pairs.
@@ -17,11 +16,35 @@ pub struct Block {
 
 impl Block {
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut b =
+            BytesMut::with_capacity(self.data.len() + SIZEOF_U16 * self.offsets.len() + SIZEOF_U16);
+        // Data
+        b.extend_from_slice(&self.data);
+        // Offsets
+        for offset in &self.offsets {
+            b.put_u16_le(*offset);
+        }
+        // num_of_elements
+        b.put_u16_le(self.offsets.len() as u16);
+        b.freeze()
     }
 
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let num_of_elements = (&data[data.len() - SIZEOF_U16..]).get_u16_le() as usize;
+
+        let data_and_offsets = &data[..data.len() - SIZEOF_U16];
+        let data_end = data.len() - SIZEOF_U16 - SIZEOF_U16 * num_of_elements;
+
+        let data = &data_and_offsets[..data_end];
+        let offsets = &data_and_offsets[data_end..];
+
+        Self {
+            data: data.to_vec(),
+            offsets: offsets
+                .chunks_exact(2)
+                .map(|mut chunk| chunk.get_u16_le())
+                .collect(),
+        }
     }
 }
 
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,30 +1,63 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use bytes::BufMut;
 
 use super::Block;
+use super::SIZEOF_U16;
 
 /// Builds a block.
-pub struct BlockBuilder {}
+pub struct BlockBuilder {
+    data: Vec<u8>,
+    offsets: Vec<u16>,
+    block_size: usize,
+    current_block_size: usize,
+}
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            data: Vec::with_capacity(block_size),
+            offsets: Default::default(),
+            block_size,
+            current_block_size: SIZEOF_U16, // num_of_elements is u16
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
-        unimplemented!()
+        assert!(!key.is_empty(), "key must not be empty");
+        assert!(key.len() < u16::MAX as usize, "key size exceeds limit");
+        assert!(value.len() < u16::MAX as usize, "value size exceeds limit");
+
+        let entry_size = SIZEOF_U16 + key.len() + SIZEOF_U16 + value.len(); // Encoded key_len & value_len are u16
+        let entry_total_size = entry_size + SIZEOF_U16; // Plus the encoded offset (u16)
+
+        // Check if the block is full
+        if !self.is_empty() && self.current_block_size + entry_total_size > self.block_size {
+            return false;
+        }
+
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16_le(key.len() as _);
+        self.data.extend(key);
+        self.data.put_u16_le(value.len() as _);
+        self.data.extend(value);
+
+        self.current_block_size += entry_total_size;
+
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.len() == 0
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
