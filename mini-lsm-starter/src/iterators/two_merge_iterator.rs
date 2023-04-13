586663200c1591277c1fac2d786d27dfdadc42d3
stamp711 <stamp1024@gmail.com>
2023-04-13 19:52:23 +0800

    Two-merge iterator

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -1,38 +1,85 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use anyhow::Result;
 
 use super::StorageIterator;
+
+enum Selection {
+    A,
+    B,
+}
 
 /// Merges two iterators of different types into one. If the two iterators have the same key, only
 /// produce the key once and prefer the entry from A.
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
-    // Add fields as need
+    current: Selection,
 }
 
 impl<A: StorageIterator, B: StorageIterator> TwoMergeIterator<A, B> {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let mut this = Self {
+            a,
+            b,
+            current: Selection::A, // Does not matter for now.
+        };
+        this.current = this.select();
+        Ok(this)
+    }
+
+    fn select(&self) -> Selection {
+        let a_key = self.a.is_valid().then_some(()).map(|()| self.a.key());
+        let b_key = self.b.is_valid().then_some(()).map(|()| self.b.key());
+        match (a_key, b_key) {
+            (None, _) => Selection::B,
+            (Some(_), None) => Selection::A,
+            (Some(ak), Some(bk)) => {
+                if ak <= bk {
+                    Selection::A
+                } else {
+                    Selection::B
+                }
+            }
+        }
     }
 }
 
 impl<A: StorageIterator, B: StorageIterator> StorageIterator for TwoMergeIterator<A, B> {
     fn key(&self) -> &[u8] {
-        unimplemented!()
+        match self.current {
+            Selection::A => self.a.key(),
+            Selection::B => self.b.key(),
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        match self.current {
+            Selection::A => self.a.value(),
+            Selection::B => self.b.value(),
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        match self.current {
+            Selection::A => self.a.is_valid(),
+            Selection::B => self.b.is_valid(),
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        match self.current {
+            Selection::A => {
+                // Need to check if B has the same key and skip.
+                let current_key = self.a.key();
+                if self.b.is_valid() && self.b.key() == current_key {
+                    self.b.next()?;
+                }
+                self.a.next()?;
+            }
+            Selection::B => {
+                self.b.next()?;
+            }
+        }
+        self.current = self.select();
+        Ok(())
     }
 }
